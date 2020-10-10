76223c721fd2f8510047e74314d4c10026a5478b
Hashininirasha <hashininirashamunasinghe@gmail.com>
2020-10-10 20:39:17 +0530

    added image folder

--- src/Back_Day.java
+++ src/Back_Day.java
@@ -1,7 +1,10 @@
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Image;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
@@ -30,11 +33,18 @@ public class Back_Day extends JFrame {
 	 */
 	public Back_Day() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 694, 427);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("");
+		Image img=new ImageIcon(this.getClass().getResource("/00IPLR68C6436F30E3D7E6px.jpg")).getImage();
+		lblNewLabel.setIcon(new ImageIcon(img));
+		lblNewLabel.setBounds(10, 11, 658, 377);
+		contentPane.add(lblNewLabel);
+		
+		
 	}
-
 }
