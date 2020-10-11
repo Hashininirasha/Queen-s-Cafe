472fb61d76339c2aa7d6678d29b30ac5fd223392
Hashininirasha <hashininirashamunasinghe@gmail.com>
2020-10-11 11:47:40 +0530

    added captions using lables

--- src/Back_Day.java
+++ src/Back_Day.java
@@ -45,9 +45,39 @@ public class Back_Day extends JFrame {
 		contentPane.setLayout(null);
 		Image img=new ImageIcon(this.getClass().getResource("/00IPLR68C6436F30E3D7E6px.jpg")).getImage();
 		
+		JLabel lblNewLabel_3 = new JLabel("food items for an evening event.Stay Home-Stay Safe");
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNewLabel_3.setForeground(Color.WHITE);
+		lblNewLabel_3.setBounds(10, 349, 406, 14);
+		contentPane.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_2 = new JLabel("and we do not accept food orders for dinner and if you are looking ");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNewLabel_2.setForeground(Color.WHITE);
+		lblNewLabel_2.setBounds(10, 324, 406, 14);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_1 = new JLabel("which is nearest to the delivery location, We will deliver lunch orders around 12 - 1.30 ");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNewLabel_1.setForeground(Color.WHITE);
+		lblNewLabel_1.setBounds(10, 299, 406, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel caption2 = new JLabel("Foods delivery is limited to below areas/cities All food items delivering from reputed Our restaurant,");
+		caption2.setFont(new Font("Tahoma", Font.BOLD, 11));
+		caption2.setForeground(Color.WHITE);
+		caption2.setBounds(10, 274, 638, 14);
+		contentPane.add(caption2);
+		
+		JLabel caption = new JLabel("Now you can order your favourite food from Queen's Cafe.");
+		caption.setFont(new Font("Tahoma", Font.BOLD, 11));
+		caption.setForeground(Color.WHITE);
+		caption.setBounds(10, 249, 312, 14);
+		contentPane.add(caption);
+		
 		JButton btnNewButton = new JButton("Go to Menu ");
 		btnNewButton.setFont(new Font("Matura MT Script Capitals", Font.PLAIN, 17));
-		btnNewButton.setBounds(459, 290, 176, 33);
+		btnNewButton.setBounds(492, 316, 176, 33);
 		contentPane.add(btnNewButton);
 		
 		JLabel lblNewLabel = new JLabel("");
